8b43e299c0298a178d653b64899774384fdd2c1b
Mahesh <itstribe.mahi@gmail.com>
2022-08-16 23:34:50 +0530

    for loop program

---
+++ forLoop.js
@@ -0,0 +1,20 @@
+// for(i=0;i<=7;i++){  //normal flow of a program
+//     // if(i==4){    //conditionn when it breaks
+//     //     break;
+//     // }
+    
+//     console.log('The elements is :' +i);
+// }
+
+
+// increment by 4
+// for(x=0; x<16; x+=4){
+//     console.log('the element is : ', +x);
+// }
+
+   
+// array  based
+let arr=['cars',123,'bikes',345,'flights'];
+for(x=0;x<arr.length;x++){
+    console.log("number is ",+x)
+}
